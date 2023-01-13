6ed20fd2cfa142dad97a645265ed0aeb29b27f54
JAMT <magallanest1905@email.com>
2023-01-13 16:33:58 -0500

    Agregando una nueva rama

---
+++ SessionPromedio/src/java/controller/LeeAlumno.java
@@ -0,0 +1,17 @@
+
+package controller;
+
+import java.util.ArrayList;
+import negocios.Alumno;
+
+/**
+ *
+ * @author JAMT
+ */
+class LeeAlumno {
+
+    static ArrayList<Alumno> leeAlumnos(String path) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
